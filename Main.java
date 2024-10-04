8f55855fd1f01297879ecfd02d75b69a8ace8f72
Rodrigo Bettio <rodriguinhobettiojr1@gmail.com>
2024-10-04 17:50:30 -0300

    Tratamento de erro que tinha no switch case

--- Main.java
+++ Main.java
@@ -43,39 +43,37 @@ public class Main {
     }
 
     public static Pessoa CadastrarPessoa() {
+        Scanner scanner = new Scanner(System.in);
+        
         String nome, nascimento, genero, CPF, logradouro, numero, bairro, cidade, estado, pais, telefone, email, hashsenha;
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Digite o nome: ");
-            nome = scanner.nextLine();
-            System.out.print("Digite a data de nascimento: ");
-            nascimento = scanner.nextLine();
-            System.out.println("Digite o gênero : ('M' para masculino, 'F' para feminino e 'O' para outros)");
-            genero = scanner.nextLine();
-            System.out.println("Digite o CPF: (ex.: 123.123.123-12)");
-            CPF = scanner.nextLine();
-            System.out.print("Digite o logradouro: ");
-            logradouro = scanner.nextLine();
-            System.out.print("Digite o número: ");
-            numero = scanner.nextLine();
-            System.out.print("Digite o bairro: ");
-            bairro = scanner.nextLine();
-            System.out.print("Digite a cidade: ");
-            cidade = scanner.nextLine();
-            System.out.print("Digite o estado: ");
-            estado = scanner.nextLine();
-            System.out.print("Digite o país: ");
-            pais = scanner.nextLine();
-            System.out.println("Digite o telefone: (ex.: '11 11111-1111')");
-            telefone = scanner.nextLine();
-            System.out.print("Digite o email: ");
-            email = scanner.nextLine();
-            System.out.println("Digite a senha: ");
-            String senha = scanner.nextLine();
-            hashsenha = generateHash(senha);
-        }
-        return new Pessoa(nome, nascimento, genero, CPF, logradouro, numero, bairro, cidade, estado, pais, telefone,
-                email, hashsenha);
-
+        System.out.print("Digite o nome: ");
+        nome = scanner.nextLine();
+        System.out.print("Digite a data de nascimento: ");
+        nascimento = scanner.nextLine();
+        System.out.println("Digite o gênero : ('M' para masculino, 'F' para feminino e 'O' para outros)");
+        genero = scanner.nextLine();
+        System.out.println("Digite o CPF: (ex.: 123.123.123-12)");
+        CPF = scanner.nextLine();
+        System.out.print("Digite o logradouro: ");
+        logradouro = scanner.nextLine();
+        System.out.print("Digite o número: ");
+        numero = scanner.nextLine();
+        System.out.print("Digite o bairro: ");
+        bairro = scanner.nextLine();
+        System.out.print("Digite a cidade: ");
+        cidade = scanner.nextLine();
+        System.out.print("Digite o estado: ");
+        estado = scanner.nextLine();
+        System.out.print("Digite o país: ");
+        pais = scanner.nextLine();
+        System.out.println("Digite o telefone: (ex.: '11 11111-1111')");
+        telefone = scanner.nextLine();
+        System.out.print("Digite o email: ");
+        email = scanner.nextLine();
+        System.out.println("Digite a senha: ");
+        String senha = scanner.nextLine();
+        hashsenha = generateHash(senha);
+        return new Pessoa(nome, nascimento, genero, CPF, logradouro, numero, bairro, cidade, estado, pais, telefone, email, hashsenha);
     }
 
     public static void VisualizarPessoa(){
